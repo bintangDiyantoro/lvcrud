c2f24d221305ad430e7c81a26d0c6a41c83e8052
Bintang Diyantoro <bintangdiyantoro@gmail.com>
2019-01-01 09:52:03 +0700

    view Read

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -3,7 +3,8 @@
 use Faker\Generator as Faker;
 
 //define the model class
-$factory->define(App\Post::class, function (Faker $faker) {
+$factory->define(App\Post::class, function(Faker $faker){
+
     return [
         'title' => $faker->sentence(10),
         'body' => $faker->paragraph(30)
---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.app')
+    
+@section('title')
+Post Page
+@endsection
+
+@section('content')
+    <h1>Post Data</h1>
+    @if(count($posts)!=0)
+        <ul>
+        @foreach($posts as $post)
+        <li>
+        <a href="">{{$post->title}}</a> 
+        </li>
+            
+        @endforeach
+        </ul>
+    @else
+        <h1>No Data Avalaible</h1>
+    @endif
+@stop
